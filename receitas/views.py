d10540bf53ba31f20eb2b95eae98d85b68d9a8f7
Matheus <mathlima1@hotmail.com.br>
2020-04-13 15:10:12 -0300

    #02 template e arquivos estaticos adicionados

--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1> Olá mundo </h1>')
+    return render(request, 'index.html')
+
+def receita(request):
+    return render(request, 'receita.html')
