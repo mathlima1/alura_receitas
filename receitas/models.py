b32db5d9c1d54fddf6f72670d129e1670d8e1ad2
Matheus <mathlima1@hotmail.com.br>
2020-04-13 15:30:42 -0300

    #03 modelo da receita adicionado

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Receita(models.Model):
+    nome_da_receita = models.CharField(max_length = 200)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField()
+    rendimento = models.CharField(max_length = 200)
+    categorias = models.TextField()
+    data_reecita = models.DateTimeField(default = datetime.now, blank = True)
